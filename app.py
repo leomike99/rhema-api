6de38a42270d8ca020788bdbfc379366ccbaea89
leomike99 <leokasongomabela@gmail.com>
2025-05-12 14:22:33 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
+
 from flask import Flask
-import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
+@app.route('/')
 def home():
     return "✅ Rhema API déployée avec succès !"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
